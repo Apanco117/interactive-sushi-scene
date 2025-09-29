1f6b28b2010622eec578380b12b82275435ebf44
David Apanco <dperezapanco@gmail.com>
2025-09-28 18:48:04 -0600

    Config de vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from "@vercel/analytics/next"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
+      <Analytics/>
     </ThemeProvider>
   </StrictMode>,
 )
